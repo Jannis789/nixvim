f65e48c4d89e972bc5e2b29ae605dd72495123d6
jannis <jannis>
2026-06-14 01:53:55 +0200

    refactor: restrict telescope keymaps to n/v mode, revert breaking keymap optimations

--- config/telescope.nix
+++ config/telescope.nix
@@ -18,20 +18,20 @@
     extensions.file-browser.enable = true;
   };
 
-  # Scope-übergreifend (alle Modi ausser insert):
+  # Scope-übergreifend (Normal & Visual Mode):
   #   <Space>p = Dateisuche, <Space>o = Codesuche
   keymaps = [
     {
       key = "<leader>p";
       action = "<cmd>Telescope find_files<CR>";
       options.desc = "Find Files";
-      mode = ["n" "v" "x" "s" "o" "t" "l"];
+      mode = [ "n" "v" ];
     }
     {
       key = "<leader>o";
       action = "<cmd>Telescope live_grep<CR>";
       options.desc = "Live Grep";
-      mode = ["n" "v" "x" "s" "o" "t" "l"];
+      mode = [ "n" "v" ];
     }
   ];
 
