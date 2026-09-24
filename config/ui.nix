892de7f448f1c035f88a48214c0c968f6796dc0b
jannis <jannis>
2026-09-24 20:14:37 +0200

    feat: enable mouse wheel scrolling (mouse=a)

--- config/ui.nix
+++ config/ui.nix
@@ -1,5 +1,10 @@
-{
-  plugins.nui.enable = true;
-  plugins.dressing.enable = true;
-  plugins.web-devicons.enable = true;
+{ ... }: {
+  config = {
+    # Maus global: Wheel scrollt Buffers und wird im pi-Terminal an pi durchgereicht
+    opts.mouse = "a";
+
+    plugins.nui.enable = true;
+    plugins.dressing.enable = true;
+    plugins.web-devicons.enable = true;
+  };
 }
